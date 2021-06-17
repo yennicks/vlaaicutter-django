624dbfcf5df6c561cb7aa8230b5118a3e70b0451
Yennick Schepers <yennicks@users.noreply.github.com>
2021-06-17 21:43:33 +0200

    BASE_DIR change

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '{{cookiecutter.project_slug}}.users'
+    '{{cookiecutter.project_slug}}.users.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
